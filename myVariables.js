5d19fd5c90ad183b6e8f8b0fd39dc4aa55be93a8
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-10-14 11:55:20 +0300

    W01D05

---
+++ myVariables.js
@@ -0,0 +1,18 @@
+let language = "JavaScript";
+
+let score = 10;
+
+console.log(language);
+console.log(score);
+
+let length = 4;
+let width = 5;
+
+console.log(`The area of the rectangle is: ${length * width}`);
+
+let num1 = 7.6;
+let num2 = 2;
+
+console.log(
+  `${num1}/ ${num2} =${Math.trunc(num1 / num2)}remainder ${num1 % num2} `
+);
