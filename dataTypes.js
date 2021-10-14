16afd0101dfb9b1588e6cfc47e3aa11a174c17e4
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-10-14 12:54:37 +0300

    W01D05

---
+++ dataTypes.js
@@ -0,0 +1,60 @@
+
+//1
+let ggg = 5.2;
+console.log(ggg);
+let namee = 'ali'
+
+console.log(namee);
+
+let arrayy = [1, 2, 34, 5];
+console.log(arrayy);
+
+let object = {'ali': 'dsff'}
+
+console.log(object);
+
+//2
+let Number = 5.2;
+
+console.log(typeof (Number));
+
+
+let name = 'ali'
+console.log(typeof (name));
+
+
+let Array = [1293, 459, 543, 3];
+
+console.log(typeof (Array));
+
+let obj = {};
+console.log(typeof (obj));
+
+//3
+
+let head =[2,'ali',50]
+console.log(head);
+
+
+//4
+
+
+
+let arrays =[
+    function fears(ali){}
+
+
+]
+
+console.log(typeof (arrays));
+//5
+
+let sey;
+let hi =null;
+
+console.log(sey);
+console.log(hi);
+
+
+
+
