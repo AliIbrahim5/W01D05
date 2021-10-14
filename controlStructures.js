56a7e5feb8a25f811e5db0729453cbff4c800f38
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-10-14 20:34:51 +0300

    W01D05

--- controlStructures.js
+++ controlStructures.js
@@ -1,6 +1,6 @@
-//1
+1
 let num1 = 3;
-let num2 = 4;
+let num2 = -50;
 let num3 = 6;
 
 //2
@@ -8,38 +8,92 @@ let num3 = 6;
 if (num1 < num2) {
   console.log(num2);
 } else if (num1 > num2) {
-  console.log(num1);
+  console.log(num2);
 } else {
   console.log("soory");
 }
 
 //3
+if (num1>num2 && num1>num3)
+{
+  if (y>z)
+  {
+    console.log(num1+ ", " + num2 + ", " +num3);
+  }
+  else
+  {
+    console.log(num1 + ", " + num3 + ", " +num2);
+  }
+}
+else if (num2>num1 && num2 >num3)
+{
+  if (num1>num3)
+  {
+    console.log(num2 + ", " + num1 + ", " +num3);
+  }
+  else
+  {
+  }
+  console.log(num2 + ", " + num3 + ", " +num1);
+} else if (num3>num1 && num3>num2)
+{
+  if (num1>num2)
+  {
+    console.log(num3 + ", " + num1 + ", " +num2);
+  }
+  else
+  {
+    console.log(num3 + ", " + num2 + ", " +num1);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//4
 
 if (num1 % 2 !== 0) {
   console.log(" 3 :odd");
-} else num2 % 2 == 0;
+} else if ( num2 % 2 === 0)
 {
   console.log(" 6 :even");
 }
 
-//4
+//5
 
 for (let i = 0; i <= 20; i++) {
   console.log(i);
 }
-//5
+//6
 let i = 0;
 while (i <= 20) {
   console.log(i);
   i++;
 }
-//6
-for (let i = 0; i <= 20; i++) {
+//7
+for (let i = 20; i >=0 ; i--) {
   console.log(i);
 }
 
-//7
-for (let i = 1; i <= 20; i++) {
+//8
+for (let i = 0; i <=20 ; i++) {
   if (i % 2 === 0) {
     console.log(i + ": even");
   } else {
@@ -47,8 +101,13 @@ for (let i = 1; i <= 20; i++) {
   }
 }
 
-//8
+//9
 
 for (let i = 0; i <= 5; i++) {
   console.log("*".repeat(i));
 }
+
+
+
+
+
